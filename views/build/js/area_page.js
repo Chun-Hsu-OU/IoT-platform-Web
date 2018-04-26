513312a9d4d289f577f522d2ce36003ec7cdf84f
ctliu <chiatungliu@gmail.com>
2018-04-27 00:21:42 +0800

    modified chart drawing

--- views/build/js/area_page.js
+++ views/build/js/area_page.js
@@ -36,14 +36,15 @@ function get_sensorhubs(){
     var body = JSON.parse(data);
     body.Items.forEach(function make(sensorgroup){
       if(sensorgroup.visible == 1){
-        $('#sensorhubs').append('<div class="animated flipInY col-lg-6 col-md-6 col-sm-12 col-xs-12" id="A"></div>');
-        $('#A').append('<div class="tile-stats" id="B"></div>');
-        $('#B').append('<div class="icon"><i class="fa fa-tasks"></i></div>');
+        $('#sensorhubs').append('<div class="animated flipInY col-lg-6 col-md-6 col-sm-12 col-xs-12" id="' + sensorgroup.groupId + '_A"></div>');
+        console.log('<div class="animated flipInY col-lg-6 col-md-6 col-sm-12 col-xs-12" id="' + sensorgroup.groupId + '_A"></div>');
+        $('#' + sensorgroup.groupId + '_A').append('<div class="tile-stats" id="' + sensorgroup.groupId + '_B"></div>');
+        $('#' + sensorgroup.groupId + '_B').append('<div class="icon"><i class="fa fa-tasks"></i></div>');
         //$('#B').append('<div class="count"></div>');
-        $('#B').append('<br>');
-        $('#B').append('<a onclick="set_g_cookie(\''+sensorgroup.groupId+'\')" href="sensorhub.html"><h3>' + sensorgroup.name + '</h3></a>');
-        $('#B').append('<p>查看詳細數據</p>');
-        $('#B').append('<br>');
+        $('#' + sensorgroup.groupId + '_B').append('<br>');
+        $('#' + sensorgroup.groupId + '_B').append('<a onclick="set_g_cookie(\''+sensorgroup.groupId+'\')" href="sensorhub.html"><h3>' + sensorgroup.name + '</h3></a>');
+        $('#' + sensorgroup.groupId + '_B').append('<p>查看詳細數據</p>');
+        $('#' + sensorgroup.groupId + '_B').append('<br>');
       }
     });
   });
