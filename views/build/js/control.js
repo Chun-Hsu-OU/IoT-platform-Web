a53f78e6801df8af7a8169e95b57fa4fd5a9e071
Chun-Hsu-OU <a9638512@gmail.com>
2018-10-11 04:31:24 +0800

    完善規則設定(綁場域且可供選擇sensorhub和控制器啟動時間)

--- views/build/js/control.js
+++ views/build/js/control.js
@@ -17,28 +17,29 @@ function getCookie(cname) {
 }
 
 
-function add_rules(TYPE, block_num, value = '數值', symbol = 'greater') {
-  if (TYPE == 'AIR_TEMPERATURE') {
-    var type = '空氣溫度&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
-  } else if (TYPE == 'AIR_HUMIDITY') {
-    var type = '空氣濕度&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
-  } else if (TYPE == 'SOIL_TEMPERATURE') {
-    var type = '土壤溫度&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
-  } else if (TYPE == 'SOIL_HUMIDITY') {
-    var type = '土壤濕度&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
-  } else if (TYPE == 'SOIL_EC') {
-    var type = '土壤電導度&nbsp;&nbsp;&nbsp;&nbsp;'
-  } else if (TYPE == 'LIGHT_INTENSITY') {
-    var type = '光照程度&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
-  } else if (TYPE == 'CO2') {
-    var type = '二氧化碳濃度 '
-  }
-  $('#add_rule_block_' + block_num).before('<div id="rule' + TYPE + '_block_' + block_num + '">' + type + '設定：' +
-    '&emsp;&emsp;<select required="required" id=' + TYPE + '_symbol_' + block_num + '><option value="greater">大於</option><option value="less">小於</option></select>' +
-    '&emsp;&emsp;<input style="text-align:center;width:70px" type="text" id="' + TYPE + '_value_' + block_num + '" value="' + value + '">' +
-    '&emsp;&emsp;<button type="button" onclick="delete_rule(\'' + TYPE + '\', \'' + block_num + '\')" class="btn btn-danger btn-sm">刪除</button>' +
+function add_rules(TYPE, block_num, name, sensorId, value = '數值', symbol = 'greater') {
+  // if (TYPE == 'AIR_TEMPERATURE') {
+  //   var type = '空氣溫度&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+  // } else if (TYPE == 'AIR_HUMIDITY') {
+  //   var type = '空氣濕度&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
+  // } else if (TYPE == 'SOIL_TEMPERATURE') {
+  //   var type = '土壤溫度&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
+  // } else if (TYPE == 'SOIL_HUMIDITY') {
+  //   var type = '土壤濕度&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
+  // } else if (TYPE == 'SOIL_EC') {
+  //   var type = '土壤電導度&nbsp;&nbsp;&nbsp;&nbsp;'
+  // } else if (TYPE == 'LIGHT_INTENSITY') {
+  //   var type = '光照程度&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'
+  // } else if (TYPE == 'CO2') {
+  //   var type = '二氧化碳濃度 '
+  // }
+  $('#add_rule_block_' + block_num).before('<div id="rule' + TYPE + '_block_' + block_num + '_' + sensorId + '">' + name + '<span id="' + TYPE + ';' + name + ';' + sensorId + '"></span>' + 
+    '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;設定：' +
+    '&emsp;&emsp;<select required="required" id="symbol"><option value="greater">大於</option><option value="less">小於</option></select>' +
+    '&emsp;&emsp;<input style="text-align:center;width:70px" type="text" id="value" value="' + value + '">' +
+    '&emsp;&emsp;<button type="button" onclick="delete_rule(\'' + TYPE + '\', \'' + block_num + '\', \'' + sensorId + '\')" class="btn btn-danger btn-sm">刪除</button>' +
     '<br><br></div>');
-  $('#' + TYPE + '_symbol_' + block_num).val(symbol).change();
+  $('#rule' + TYPE + '_block_' + block_num + '_' + sensorId).children('#symbol').val(symbol).change();
 }
 
 function change_rules() {
@@ -55,16 +56,23 @@ function change_rules() {
     var children_element = document.getElementById(current_block).children[0].children;
 
     for (let i = 0; i < children_element.length - 1; i++) {
-      var id = children_element[i].id;
-      var type = id.slice(4).split('_block_')[0];
-      var value = document.getElementById(type + '_value_' + block_array[block_num].toString()).value;
+      var id = children_element[i].children[0].id;
+      var type = id.split(';')[0];
+      var name = id.split(';')[1];
+      var sensorId = id.split(';')[2];
+      var value = $('#rule' + type + '_block_' + block_array[block_num].toString() + '_' + sensorId).children('#value').val();
+      var symbol = $('#rule' + type + '_block_' + block_array[block_num].toString() + '_' + sensorId).children('#symbol').val();
+      var duration = $('#add_rule_block_' + block_array[block_num].toString()).children('#duration').val();
       //console.log(document.getElementById(type + '_bot_' + block_array[block_num].toString()).value);
       //console.log(type);
       var new_rule = {
-        "block": parseFloat(block_array[block_num]),
+        "block": parseInt(block_array[block_num]),
+        "name": name,
         "type": type,
-        "value": parseFloat(value),
-        "symbol": document.getElementById(type + '_symbol_' + block_array[block_num].toString()).value
+        "sensorId": sensorId,
+        "value": parseInt(value),
+        "symbol": symbol,
+        "duration": parseInt(duration)
       };
       rules.push(new_rule);
     }
@@ -73,20 +81,22 @@ function change_rules() {
   $.post(api_url + 'api/control/rules', {
     "controllerId": controller,
     "status": rules
+  }, function(){
+      alert("規則設定完成！");
   });
 
-  $.post(api_url + 'api/control/rule/length', {
-    "controllerId": controller,
-    "status": rules.length
-  });
+  // $.post(api_url + 'api/control/rule/length', {
+  //   "controllerId": controller,
+  //   "status": rules.length
+  // });
   //console.log(rules);
 }
 
-function delete_rule(type, block_num) {
-  $("#rule" + type + '_block_' + block_num).remove();
+function delete_rule(type, block_num, sensorId) {
+  $("#rule" + type + '_block_' + block_num + '_' + sensorId).remove();
 }
 
-function add_rule_block(block_num = 0, groups = 0) {
+function add_rule_block(block_num = 0, duration = 0) {
   //按新增區塊用
   if (block_num == 0) {
     var children = document.getElementById("control-tab-1").children;
@@ -102,48 +112,104 @@ function add_rule_block(block_num = 0, groups = 0) {
 
     block_num ++;
   }
-  
-  for (let group_num = 0; group_num < groups.Count; group_num++) {
-    $.get(api_url + 'api/sensors_in_group/' + groups.Items[group_num].groupId, function(data) {
-      var sensors = JSON.parse(data);
-      for (let sensor_num = 0; sensor_num < sensors.Count; sensor_num++) {
-        console.log(sensors.Items[sensor_num].sensorId);
-      }
-    });
-  }
 
   console.log(block_num);
   var id_name = "rule_block_" + block_num.toString();
 
   $('#submit_change_rule').before('<div id="temp_id">' +
     '<div style="border-width:3px;border-style:dashed;border-color:#339933;padding:5px;">' +
-    '<div class="btn-group" id="add_rule_block_' + block_num + '">' +
-      '<button type="button" class="btn btn-success">新增規則</button>' +
-      '<button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-expanded="false">' +
-        '<span class="caret"></span>' +
-        '<span class="sr-only">Toggle Dropdown</span>' +
-      '</button>' +
-      '<button type="button" class="btn btn-danger" onclick="delete_rule_block(\'' + id_name + '\')" style="float:right">刪除區塊</button>' +
-      '<ul class="dropdown-menu" role="menu">' +
-        '<li><a href="#" onclick="add_rules(\'AIR_TEMPERATURE\', \'' + block_num + '\')">空氣溫度</a></li>' +
-        '<li><a href="#" onclick="add_rules(\'AIR_HUMIDITY\', \'' + block_num + '\')">空氣濕度</a></li>' +
-        '<li class="divider"></li>' +
-        '<li><a href="#" onclick="add_rules(\'SOIL_TEMPERATURE\', \'' + block_num + '\')">土壤溫度</a></li>' +
-        '<li><a href="#" onclick="add_rules(\'SOIL_HUMIDITY\', \'' + block_num + '\')">土壤濕度</a></li>' +
-        '<li><a href="#" onclick="add_rules(\'SOIL_EC\', \'' + block_num + '\')">土壤電導度</a></li>' +
-        '<li class="divider"></li>' +
-        '<li><a href="#" onclick="add_rules(\'LIGHT_INTENSITY\', \'' + block_num + '\')">光照程度</a></li>' +
-        '<li><a href="#" onclick="add_rules(\'CO2\', \'' + block_num + '\')">二氧化碳濃度</a></li>' +
-      '</ul>' +
+    '<div id="add_rule_block_' + block_num + '"><hr>' +
+      '啟動時間:&nbsp&nbsp' + 
+      '<select id="duration">'+
+        '<option value="1">1分鐘</option>'+
+        '<option value="5">5分鐘</option>'+
+        '<option value="10">10分鐘</option>'+
+        '<option value="15">15分鐘</option>'+
+        '<option value="20">20分鐘</option>'+
+        '<option value="25">25分鐘</option>'+
+        '<option value="30">30分鐘</option>'+
+        '<option value="35">35分鐘</option>'+
+        '<option value="40">40分鐘</option>'+
+        '<option value="45">45分鐘</option>'+
+        '<option value="50">50分鐘</option>'+
+        '<option value="55">55分鐘</option>'+
+        '<option value="60">60分鐘</option>'+
+      '</select><br><br>'+
+      '選擇Sensorhub:&nbsp&nbsp<select id="show_groups_block_' + block_num + '" onchange="group_select_change(' + block_num + ')">' + '</select><br><br>' +
+      '<div class="btn-group">' +
+        '<button type="button" class="btn btn-success">新增規則</button>' +
+        '<button type="button" class="btn btn-success dropdown-toggle" data-toggle="dropdown" aria-expanded="false">' +
+          '<span class="caret"></span>' +
+          '<span class="sr-only">Toggle Dropdown</span>' +
+        '</button>' +
+        '<button type="button" class="btn btn-danger" onclick="delete_rule_block(\'' + id_name + '\')" style="float:right">刪除區塊</button>' +
+        '<ul class="dropdown-menu" role="menu" id="show_sensors_block_' + block_num + '">' +
+          // '<li><a href="#" onclick="add_rules(\'AIR_TEMPERATURE\', \'' + block_num + '\')">空氣溫度</a></li>' +
+          // '<li><a href="#" onclick="add_rules(\'AIR_HUMIDITY\', \'' + block_num + '\')">空氣濕度</a></li>' +
+          // '<li class="divider"></li>' +
+          // '<li><a href="#" onclick="add_rules(\'SOIL_TEMPERATURE\', \'' + block_num + '\')">土壤溫度</a></li>' +
+          // '<li><a href="#" onclick="add_rules(\'SOIL_HUMIDITY\', \'' + block_num + '\')">土壤濕度</a></li>' +
+          // '<li><a href="#" onclick="add_rules(\'SOIL_EC\', \'' + block_num + '\')">土壤電導度</a></li>' +
+          // '<li class="divider"></li>' +
+          // '<li><a href="#" onclick="add_rules(\'LIGHT_INTENSITY\', \'' + block_num + '\')">光照程度</a></li>' +
+          // '<li><a href="#" onclick="add_rules(\'CO2\', \'' + block_num + '\')">二氧化碳濃度</a></li>' +
+        '</ul>' +
+      '</div>' + 
     '</div>' +
   '</div><br>' +
   '</div>');
   $('#temp_id').attr("id", id_name);
+
+  //代表來自資料庫的啟動時間，只在新增block時設定一次
+  if(duration != 0){
+    $('#add_rule_block_' + block_num).children('#duration').val(duration);
+  }
+
+  load_groups_in_select_rule(block_num);
+  // load_sensors_in_select_rule(block_num);
+}
+
+function load_groups_in_select_rule(block_num){
+  $.get(api_url + 'api/sensorgroup_in_area/' + getCookie("area"), function(data) {
+    var groups = JSON.parse(data);
+    for (let group_num = 0; group_num < groups.Count; group_num++) {
+      $("#show_groups_block_"+block_num).append('<option value="' + groups.Items[group_num].groupId + '">' + groups.Items[group_num].name + '</option>');
+      //初始化 -> load 第一組 sensorhub 裡的 sensors 
+      if(group_num == 0){
+        load_sensors_in_select_rule(block_num, groups.Items[group_num].groupId);
+      }
+    }
+  });
+}
+
+function load_sensors_in_select_rule(block_num, groupId){
+  $("#show_sensors_block_"+block_num).empty();
+  $.get(api_url + 'api/sensors_in_group/' + groupId, function(data) {
+    var sensors = JSON.parse(data);
+    for (let sensor_num = 0; sensor_num < sensors.Count; sensor_num++) {
+      console.log(sensors.Items[sensor_num].sensorId);
+      var type = sensors.Items[sensor_num].sensorType;
+      var name = sensors.Items[sensor_num].name;
+      var sensorId = sensors.Items[sensor_num].sensorId;
+      $("#show_sensors_block_"+block_num).append('<li><a href="#" name="' + sensors.Items[sensor_num].sensorId + 
+      '" onclick="add_rules(\'' + type + '\', \'' + block_num + '\', \'' + name + '\', \'' + sensorId + '\')">' + name + '</a></li>');
+    }
+  });
+}
+
+function group_select_change(block_num){
+  var groupId = $("#show_groups_block_"+block_num).val();
+  console.log("change to: "+groupId);
+  load_sensors_in_select_rule(block_num, groupId);
 }
 
 function delete_rule_block(id_name) {
   $("#" + id_name).remove();
 }
+
+// $("#show_groups_block_2").children().children().click(function(){
+//   console.log(this.value);
+// });
 
 function check_children(check) {
   var children = document.getElementById("control-tab-1").children;
@@ -174,6 +240,7 @@ function block_change(block_array) {
 function add_clock(clock_record = 0){
   var clock_group = $("#clock_group").children();
   var clock_num = 1;
+  //如果已經有鬧鐘，就算最後一個鬧鐘的clock_num(最大的clock_num) + 1
   if(clock_group.length != 0){
     clock_num = parseInt(clock_group[clock_group.length - 1].id.slice(6)) + 1;
   }
@@ -181,7 +248,7 @@ function add_clock(clock_record = 0){
   $("#clock_group").append('<div id="clock_'+ clock_num +'">'+
     '<div class="col-sm-3">'+
       '選擇星期:&nbsp&nbsp'+
-      '<select>'+
+      '<select id="weekday">'+
         '<option value="0">星期一</option>'+
         '<option value="1">星期二</option>'+
         '<option value="2">星期三</option>'+
@@ -196,7 +263,7 @@ function add_clock(clock_record = 0){
     '</div>'+
     '<div class="col-sm-3">'+
       '<div class="input-group date timepicker">'+
-        '<input type="text" class="form-control" value="10:00 AM"/>'+
+        '<input type="text" class="form-control" id="start" value="10:00 AM"/>'+
         '<span class="input-group-addon">'+
           '<span class="glyphicon glyphicon-time"></span>'+
         '</span>'+
@@ -205,7 +272,7 @@ function add_clock(clock_record = 0){
     '<div class="col-sm-1"></div>'+
     '<div class="col-sm-3">'+
       '啟動時間:&nbsp&nbsp'+
-      '<select>'+
+      '<select id="duration">'+
         '<option value="1">1分鐘</option>'+
         '<option value="5">5分鐘</option>'+
         '<option value="10">10分鐘</option>'+
@@ -226,12 +293,11 @@ function add_clock(clock_record = 0){
     '<hr>'+
   '</div>');
 
-  //表示是來自資料庫的鬧鐘設定
+  //代表來自資料庫的鬧鐘設定
   if(clock_record != 0){
-    var clock_group = $("#clock_"+clock_num).children();
-    clock_group[0].children[0].value = clock_record.weekday;
-    clock_group[2].children[0].children[0].value = clock_record.start;
-    clock_group[4].children[0].value = clock_record.duration;
+    $("#clock_"+clock_num).find("#weekday").val(clock_record.weekday);
+    $("#clock_"+clock_num).find("#start").val(clock_record.start);
+    $("#clock_"+clock_num).find("#duration").val(clock_record.duration);
   }
 
   //一定要加，讓新加進來的timepicker也可以選時間
@@ -250,9 +316,9 @@ function submit_clock(){
   for(let i=0; i<clock_group.length; i++){
     var clock_items = clock_group[i].children;
     var new_clock = {
-      "weekday": clock_items[0].children[0].value,
-      "start": clock_items[2].children[0].children[0].value,
-      "duration": clock_items[4].children[0].value
+      "weekday": clock_group[i].querySelector("#weekday").value,
+      "start": clock_group[i].querySelector("#start").value,
+      "duration": parseInt(clock_group[i].querySelector("#duration").value)
     };
     clocks.push(new_clock);
   }
@@ -278,3 +344,5 @@ function submit_duration(){
   });
 }
 //------ 手動 ------
+
+
