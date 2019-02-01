c7ac92d637dcd46c11d0698fca4458410c5f4c49
Chun-Hsu-OU <a9638512@gmail.com>
2019-02-01 23:22:48 +0800

    匯出日誌(phpspreadsheet測試)

--- views/log_system/export_logs.php
+++ views/log_system/export_logs.php
@@ -1,6 +1,9 @@
 <?php
-// 引入 PHPExcel 函式庫
-include_once "PHPExcel-1.8/Classes/PHPExcel.php";
+require 'vendor/autoload.php';
+
+// 引入 namespace
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 //設定時區
 date_default_timezone_set('Asia/Taipei');
@@ -32,41 +35,14 @@ function logs_in_daterange($url){
     return $result;
 }
 
-//接著我們要生成一個 PHPExcel 物件來幫忙我們處理 Excel 檔案
-$objPHPExcel = new PHPExcel();
-
-// 將活頁簿裏的第一張工作表設為要操作的工作表
-$objPHPExcel->setActiveSheetIndex(0);
-
-// 取得工作表物件
-$objWorksheet = $objPHPExcel->getActiveSheet();
-
-// 將值填入儲存格中
-$objWorksheet->setCellValue("A1","記錄者");
-$objWorksheet->setCellValue("B1","場域");
-$objWorksheet->setCellValue("C1","作物");
-$objWorksheet->setCellValue("D1","工作事項");
-$objWorksheet->setCellValue("E1","農機具");
-$objWorksheet->setCellValue("F1","病蟲害");
-$objWorksheet->setCellValue("G1","備忘錄");
-$objWorksheet->setCellValue("H1","記錄時間");
-
-// 輸出 header
-header("Content-Type: application/vnd.ms-excel");
-header("Content-Disposition: attachment;filename=test_01.xlsx");
-
-$objWriter = PHPExcel_IOFactory::createWriter($objPHPExcel, 'Excel2007');
-// 清除輸出緩衝區內容後，直接輸出成自動下載檔案
-ob_end_clean();
-$objWriter->save('php://output');
-
-
-
+// 创建
+$spreadsheet = new Spreadsheet();
+$sheet = $spreadsheet->getActiveSheet();
 
 // 取得相關參數(使用者id、匯出日誌開始時間、匯出日誌結束時間)
-// $ownerId = $_POST["ownerId"];
-// $fromDate = $_POST["fromDate"];
-// $toDate = $_POST["toDate"];
+$ownerId = $_POST["ownerId"];
+$fromDate = $_POST["fromDate"];
+$toDate = $_POST["toDate"];
 
 // console_log($public['api_url']);
 // console_log($_COOKIE['token']);
@@ -75,32 +51,39 @@ $objWriter->save('php://output');
 // console_log($_POST["toDate"]);
 
 // 取得檔案名稱
-// $fileName = $_POST["filename"].".xlsx";
+$fileName = $_POST["filename"].".xlsx";
 // $fileName = "測試.xlsx";
 
 //將查到的日誌一列一列寫到excel
-// $sheet->setCellValueByColumnAndRow(1, 1, '記錄者');
-// $sheet->setCellValueByColumnAndRow(2, 1, '場域');
-// $sheet->setCellValueByColumnAndRow(3, 1, '作物');
-// $sheet->setCellValueByColumnAndRow(4, 1, '工作事項');
-// $sheet->setCellValueByColumnAndRow(5, 1, '農機具');
-// $sheet->setCellValueByColumnAndRow(6, 1, '病蟲害');
-// $sheet->setCellValueByColumnAndRow(7, 1, '備忘錄');
-// $sheet->setCellValueByColumnAndRow(8, 1, '記錄時間');
+$sheet->setCellValueByColumnAndRow(1, 1, '記錄者');
+$sheet->setCellValueByColumnAndRow(2, 1, '場域');
+$sheet->setCellValueByColumnAndRow(3, 1, '作物');
+$sheet->setCellValueByColumnAndRow(4, 1, '工作事項');
+$sheet->setCellValueByColumnAndRow(5, 1, '農機具');
+$sheet->setCellValueByColumnAndRow(6, 1, '病蟲害');
+$sheet->setCellValueByColumnAndRow(7, 1, '備忘錄');
+$sheet->setCellValueByColumnAndRow(8, 1, '記錄時間');
 
-// $str = logs_in_daterange($public['api_url']."api/export/log/$ownerId/$fromDate/$toDate");
+$str = logs_in_daterange($public['api_url']."api/export/log/$ownerId/$fromDate/$toDate");
 // $str = logs_in_daterange($public['api_url']."api/export/log/557f27e1-e918-41dd-b044-f431b718b0e1/1535731200000/1538319600000");
-// // echo $str;
-// $logs = json_decode($str);
-// for($i=0;$i<count($logs->Items);$i++){
-//     $sheet->setCellValueByColumnAndRow(1, $i+2, $logs->Items[$i]->author);
-//     $sheet->setCellValueByColumnAndRow(2, $i+2, $logs->Items[$i]->area);
-//     $sheet->setCellValueByColumnAndRow(3, $i+2, $logs->Items[$i]->crop);
-//     $sheet->setCellValueByColumnAndRow(4, $i+2, $logs->Items[$i]->type);
-//     $sheet->setCellValueByColumnAndRow(5, $i+2, $logs->Items[$i]->machine);
-//     $sheet->setCellValueByColumnAndRow(6, $i+2, $logs->Items[$i]->diseases);
-//     $sheet->setCellValueByColumnAndRow(7, $i+2, $logs->Items[$i]->memo);
-//     $sheet->setCellValueByColumnAndRow(8, $i+2, date("Y/m/d H:i:s",$logs->Items[$i]->set_time));
-// }
+// echo $str;
+$logs = json_decode($str);
+for($i=0;$i<count($logs->Items);$i++){
+    $sheet->setCellValueByColumnAndRow(1, $i+2, $logs->Items[$i]->author);
+    $sheet->setCellValueByColumnAndRow(2, $i+2, $logs->Items[$i]->area);
+    $sheet->setCellValueByColumnAndRow(3, $i+2, $logs->Items[$i]->crop);
+    $sheet->setCellValueByColumnAndRow(4, $i+2, $logs->Items[$i]->type);
+    $sheet->setCellValueByColumnAndRow(5, $i+2, $logs->Items[$i]->machine);
+    $sheet->setCellValueByColumnAndRow(6, $i+2, $logs->Items[$i]->diseases);
+    $sheet->setCellValueByColumnAndRow(7, $i+2, $logs->Items[$i]->memo);
+    $sheet->setCellValueByColumnAndRow(8, $i+2, date("Y/m/d H:i:s",$logs->Items[$i]->set_time));
+}
 
+//告诉浏览器输出07Excel文件
+header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+//告诉浏览器输出文件名称
+header('Content-Disposition: attachment;filename="'.$fileName.'"');
+
+$writer = new Xlsx($spreadsheet);
+$writer->save('php://output');
 ?>
