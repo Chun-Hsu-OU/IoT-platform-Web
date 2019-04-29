80f6dc6ec488c5e1cb180e22032d136045baaad7
Chun-Hsu-OU <a9638512@gmail.com>
2019-04-29 16:54:59 +0800

    關掉病蟲害gmail通知

--- views/log_system/insert_process.php
+++ views/log_system/insert_process.php
@@ -104,31 +104,31 @@
             sendLineNotify($initData, $token);
 
             /* 寄出email */
-            $C_email=$_POST["email"];
-            $mail= new PHPMailer();                          //建立新物件
-            $mail->IsSMTP();                                    //設定使用SMTP方式寄信
-            $mail->SMTPAuth = true;                        //設定SMTP需要驗證
-            $mail->SMTPSecure = "ssl";                    // Gmail的SMTP主機需要使用SSL連線
-            // $mail->SMTPDebug = 2;          
-            $mail->Host = "smtp.gmail.com";             //Gamil的SMTP主機
-            $mail->Port = 465;                                 //Gamil的SMTP主機的埠號(Gmail為465)。
-            $mail->CharSet = "utf-8";                       //郵件編碼
-            $mail->Username = "hsnl33564@gmail.com"; //Gamil帳號
-            $mail->Password = "Hsnl-iot33564";                 //Gmail密碼
-            $mail->From = "hsnl33564@gmail.com";        //寄件者信箱
-            $mail->FromName = "清華大學智慧農業物聯網平台"; //寄件者姓名
-            $mail->Subject ="未知病蟲害即時通報"; //郵件標題
-            $mail->Body = "<b style='color:blue'>場域：</b><br>".$_POST["area"]."<br><br><b style='color:blue'>作物：</b><br>".$_POST["crop"]."<br><br><b style='color:blue'>病情描述：</b><br>".$_POST["description"]; //郵件內容
-            $mail->IsHTML(true);                             //郵件內容為html
-            $mail->AddAddress($C_email);            //收件者郵件及名稱
-            if(!$mail->Send()){
-                echo "Error: " . $mail->ErrorInfo;
-                echo "Message was not sent<br/ >";
-            }else{
-                echo "<script>
-                    alert('未知病蟲害通報已送出!');
-                </script>";
-            }
+            // $C_email=$_POST["email"];
+            // $mail= new PHPMailer();                          //建立新物件
+            // $mail->IsSMTP();                                    //設定使用SMTP方式寄信
+            // $mail->SMTPAuth = true;                        //設定SMTP需要驗證
+            // $mail->SMTPSecure = "ssl";                    // Gmail的SMTP主機需要使用SSL連線
+            // // $mail->SMTPDebug = 2;          
+            // $mail->Host = "smtp.gmail.com";             //Gamil的SMTP主機
+            // $mail->Port = 465;                                 //Gamil的SMTP主機的埠號(Gmail為465)。
+            // $mail->CharSet = "utf-8";                       //郵件編碼
+            // $mail->Username = "hsnl33564@gmail.com"; //Gamil帳號
+            // $mail->Password = "Hsnl-iot33564";                 //Gmail密碼
+            // $mail->From = "hsnl33564@gmail.com";        //寄件者信箱
+            // $mail->FromName = "清華大學智慧農業物聯網平台"; //寄件者姓名
+            // $mail->Subject ="未知病蟲害即時通報"; //郵件標題
+            // $mail->Body = "<b style='color:blue'>場域：</b><br>".$_POST["area"]."<br><br><b style='color:blue'>作物：</b><br>".$_POST["crop"]."<br><br><b style='color:blue'>病情描述：</b><br>".$_POST["description"]; //郵件內容
+            // $mail->IsHTML(true);                             //郵件內容為html
+            // $mail->AddAddress($C_email);            //收件者郵件及名稱
+            // if(!$mail->Send()){
+            //     echo "Error: " . $mail->ErrorInfo;
+            //     echo "Message was not sent<br/ >";
+            // }else{
+            //     echo "<script>
+            //         alert('未知病蟲害通報已送出!');
+            //     </script>";
+            // }
         }
         
         // echo $set_time."<br>";
